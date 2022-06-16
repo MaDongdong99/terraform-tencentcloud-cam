68545cefadb0368e1ea8dea22f1b1959c76b2afa
madongdong <donaldma@tencent.com>
2022-06-16 20:42:33 +0800

    update for private cloud

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "tencentcloud_provider_domain" {
+  default = "tencentcloudapi.com"
+}
+variable "tencentcloud_provider_protocol" {
+  default = "HTTPS"
+}
+variable "tencentcloud_provider_region" {
+  default = "ap-guangzhou"
+}
 
 variable "user_default_remark" {
   default = ""
