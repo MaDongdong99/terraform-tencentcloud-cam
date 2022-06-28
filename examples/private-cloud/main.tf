e852daa2db6642965bf0c4806e87cab9a3e9fabc
madongdong <donaldma@tencent.com>
2022-06-28 14:59:58 +0800

    remove provider from module

--- examples/private-cloud/main.tf
+++ examples/private-cloud/main.tf
@@ -1,16 +1,20 @@
+provider "tencentcloud" {
+  domain = "api3.yun.com"
+  protocol = "HTTP"
+  region = "jj"
+}
+
 
 module "policies" {
   source = "../.."
-  tencentcloud_provider_domain = "api3.yun.com"
-  tencentcloud_provider_protocol = "HTTP"
-  tencentcloud_provider_region = "tencentcloud"
 
-  groups = [
-    { name = "test-group-1", remark = "test",
-      users = ["cam-user-test-aaa", "cam-user-test-bbb"]
-      policy_names = ["ProjectFullAccess", "QCloudCmqQueueCreaterFullAccess", "ProjectReadOnlyAccess"]
-    }
-  ]
+
+//  groups = [
+//    { name = "test-group-1", remark = "test",
+////      users = ["cam-user-test-aaa", "cam-user-test-bbb"]
+//      policy_names = ["ProjectFullAccess", "QCloudCmqQueueCreaterFullAccess", "ProjectReadOnlyAccess"]
+//    }
+//  ]
 
   users = [
     { name = "cam-user-test-aaa", remark = "test", use_api = true, force_delete = true }
